413fc33cbecea2a2ebbb9ff1846a4057cfad6b16
vanamayorov <denatarelcom2105@gmail.com>
2021-01-25 02:05:14 +0200

    version 1.1

--- practice_c++/prjct.cpp
+++ practice_c++/prjct.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 #define ORDINARY_SIZE 33
+//#define DEBUG
 bool guessANumberGame(int* deposit);
 void printMessage(string message, bool printTop, bool printBottom, int numbers);
 void drawHangMan(int hangCounts);
@@ -19,6 +20,11 @@ int triesLeft(string word, string guessed);
 void alterDeposit(int* deposit, bool win);
 void depositCheck(int* deposit);
 bool validateString(string& name);
+void drawArray(char array[][3]);
+void inputPosition(char array[][3]);
+void togglePlayer(char array[][3]);
+char checkWin(char array[][3]);
+int discoverWinner(char validation);
 int main() {
 	system("color F2");
 	setlocale(LC_ALL, "Rus");
@@ -36,9 +42,9 @@ int main() {
 	cout << "Please enter your name:" << endl << "> ";
 	getline(cin, name);
 	stringValidated = validateString(name);
-	while (name.length() > MAX_SIZE_OF_NAME || name.empty() || !stringValidated) { 
-		printMessage("Please enter your name", true, false, ORDINARY_SIZE+5);
-		printMessage("name cannot be larger than 50 symbols", false, true, ORDINARY_SIZE+5);
+	while (name.length() > MAX_SIZE_OF_NAME || name.empty() || !stringValidated) {
+		printMessage("Please enter your name", true, false, ORDINARY_SIZE + 5);
+		printMessage("name cannot be larger than 50 symbols", false, true, ORDINARY_SIZE + 5);
 		cout << "> ";
 		getline(cin, name);
 		stringValidated = validateString(name);
@@ -49,12 +55,12 @@ int main() {
 	do {
 		depositCheck(&deposit);
 		cout << name << ", your balance is " << deposit << '$' << endl;
-		cout << "List of games: \n 1 - Guess a number\n 2 - Hangman game\n 3 - Exit" << endl << "> ";
-		while (!(cin >> game) ||  (game != 1 && game != 2 && game != 3)) {
+		cout << "List of games: \n 1 - Guess a number\n 2 - Hangman game\n 3 - TicTacToe\n 4 - Exit" << endl << "> ";
+		while (!(cin >> game) || (game != 1 && game != 2 && game != 3 && game != 4)) {
 			cin.clear();
 			cin.ignore(INT_MAX, '\n');
 			cout << "Invalid type of number!" << endl;
-			cout << "List of games: \n 1 - Guess a number\n 2 - Hangman game\n 3 - Exit" << endl << "> ";
+			cout << "List of games: \n 1 - Guess a number\n 2 - Hangman game\n 3 - TicTacToe\n 4 - Exit" << endl << "> ";
 		}
 		chooseGame(game, &deposit, &exitCondition);
 		if (exitCondition) {
@@ -149,12 +155,341 @@ void chooseGame(int number, int* deposit, bool* exitCondition) {
 		system("cls");
 		break;
 	case 3:
+		do {
+			char array[3][3] = { {'1', '2', '3' }, {'4', '5', '6'}, {'7', '8', '9'} };
+			int n = 0;
+			char validation = '/';
+			system("cls");
+			printMessage("YOU CHOOSE TICTACTOE", true, true, ORDINARY_SIZE);
+			while (n < 9 && validation == '/') {
+				inputPosition(array);
+				validation = checkWin(array);
+				if (discoverWinner(validation) == 1) {
+					printMessage("PLAYER 1 IS A WINNER", true, false, ORDINARY_SIZE);
+					alterDeposit(deposit, true);
+					printMessage("YOUR AWARD IS +100$", false, true, ORDINARY_SIZE);
+					break;
+				}
+				else if (discoverWinner(validation) == 2) {
+					printMessage("PLAYER 2 IS A WINNER", true, false, ORDINARY_SIZE);
+					alterDeposit(deposit, true);
+					printMessage("YOUR AWARD IS +100$", false, true, ORDINARY_SIZE);
+					break;
+				}
+				else {
+					n++;
+#ifdef DEBUG
+					cout << "Score" << n << endl;
+#endif
+				}
+				if (n == 9) {
+					printMessage("IT`S A DRAW!", true, false, ORDINARY_SIZE);
+					alterDeposit(deposit, false);
+					printMessage("YOU ARE NOT LUCKY TODAY -100$", false, true, ORDINARY_SIZE);
+					break;
+				}
+				togglePlayer(array);
+				validation = checkWin(array);
+				if (discoverWinner(validation) == 1) {
+					printMessage("PLAYER 1 IS A WINNER", true, false, ORDINARY_SIZE);
+					alterDeposit(deposit, true);
+					printMessage("YOUR AWARD IS +100$", false, true, ORDINARY_SIZE);
+					break;
+				}
+				else if (discoverWinner(validation) == 2) {
+					printMessage("PLAYER 2 IS A WINNER", true, false, ORDINARY_SIZE);
+					alterDeposit(deposit, true);
+					printMessage("YOUR AWARD IS +100$", false, true, ORDINARY_SIZE);
+					break;
+				}
+				else {
+					n++;
+#ifdef DEBUG
+					cout << "Score" << n << endl;
+#endif
+				}
+
+			if (n == 9) {
+				printMessage("IT`S A DRAW!", true, false, ORDINARY_SIZE);
+				alterDeposit(deposit, false);
+				printMessage("YOU ARE NOT LUCKY TODAY -100$", false, true, ORDINARY_SIZE);
+				break;
+			}
+			}
+			cout << "Would you like to continue playing a tictactoe game?(y/Y - yes)" << endl << "> ";
+			cin >> response;
+			system("cls");
+		} while (response == 'y' || response == 'Y');
+		break;
+	case 4:
 		*exitCondition = true;
 		break;
 	default:
 		cout << "Invalid type of number!" << endl;
 		break;
 	}
+}
+
+int discoverWinner(char validation) {
+	if (validation == 'X') {
+		return 1;
+	}
+	else if (validation == 'O') {
+		return 2;
+	}
+	return 0;
+}
+char checkWin(char array[][3]) {
+
+	if (array[0][0] == 'X' && array[0][1] == 'X' && array[0][2] == 'X') {
+		return 'X';
+	}
+	else if (array[1][0] == 'X' && array[1][1] == 'X' && array[1][2] == 'X') {
+		return 'X';
+	}
+	else if (array[2][0] == 'X' && array[2][1] == 'X' && array[2][2] == 'X') {
+		return 'X';
+	}
+	else if (array[0][0] == 'X' && array[1][0] == 'X' && array[2][0] == 'X') {
+		return 'X';
+	}
+	else if (array[0][1] == 'X' && array[1][1] == 'X' && array[2][1] == 'X') {
+		return 'X';
+	}
+	else if (array[0][2] == 'X' && array[1][2] == 'X' && array[2][2] == 'X') {
+		return 'X';
+	}
+	else if (array[0][0] == 'X' && array[1][1] == 'X' && array[2][2] == 'X') {
+		return 'X';
+	}
+	else if (array[2][0] == 'X' && array[1][1] == 'X' && array[0][2] == 'X') {
+		return 'X';
+	}
+	if (array[0][0] == 'O' && array[0][1] == 'O' && array[0][2] == 'O') {
+		return 'O';
+	}
+	else if (array[1][0] == 'O' && array[1][1] == 'O' && array[1][2] == 'O') {
+		return 'O';
+	}
+	else if (array[2][0] == 'O' && array[2][1] == 'O' && array[2][2] == 'O') {
+		return 'O';
+	}
+	else if (array[0][0] == 'O' && array[1][0] == 'O' && array[2][0] == 'O') {
+		return 'O';
+	}
+	else if (array[0][1] == 'O' && array[1][1] == 'O' && array[2][1] == 'O') {
+		return 'O';
+	}
+	else if (array[0][2] == 'O' && array[1][2] == 'O' && array[2][2] == 'O') {
+		return 'O';
+	}
+	else if (array[0][0] == 'O' && array[1][1] == 'O' && array[2][2] == 'O') {
+		return 'O';
+	}
+	else if (array[2][0] == 'O' && array[1][1] == 'O' && array[0][2] == 'O') {
+		return 'O';
+	}
+	return '/';
+}
+void togglePlayer(char array[][3]) {
+	int position;
+	cout << "Player 2" << endl;
+	do {
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		drawArray(array);
+		cout << "Choose Position:" << endl << "> ";
+		cin >> position;
+
+	} while (!cin.good() || position > 9 || position < 1);
+	switch (position) {
+	case 1:
+		if (array[0][0] == 'X' || array[0][0] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[0][0] = 'O';
+		}
+		break;
+	case 2:
+		if (array[0][1] == 'X' || array[0][1] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[0][1] = 'O';
+		}
+
+		break;
+	case 3:
+		if (array[0][2] == 'X' || array[0][2] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[0][2] = 'O';
+		}
+		break;
+	case 4:
+		if (array[1][0] == 'X' || array[1][0] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[1][0] = 'O';
+		}
+		break;
+	case 5:
+		if (array[1][1] == 'X' || array[1][1] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[1][1] = 'O';
+		}
+		break;
+	case 6:
+		if (array[1][2] == 'X' || array[1][2] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[1][2] = 'O';
+		}
+		break;
+	case 7:
+		if (array[2][0] == 'X' || array[2][0] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[2][0] = 'O';
+		}
+
+		break;
+	case 8:
+		if (array[2][1] == 'X' || array[2][1] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[2][1] = 'O';
+		}
+
+		break;
+	case 9:
+		if (array[2][2] == 'X' || array[2][2] == 'O') {
+			cout << "This position is occupied, try again." << endl;
+			togglePlayer(array);
+		}
+		else {
+			array[2][2] = 'O';
+		}
+
+		break;
+	}
+
+}
+void inputPosition(char array[][3]) {
+	int position;
+	cout << "Player 1" << endl;
+	do {
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		drawArray(array);
+		cout << "Choose Position:" << endl << "> ";
+		cin >> position;
+
+	} while (!cin.good() || position > 9 || position < 1);
+	switch (position) {
+	case 1:
+		if (array[0][0] == 'O' || array[0][0] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[0][0] = 'X';
+		}
+		break;
+	case 2:
+		if (array[0][1] == 'O' || array[0][1] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[0][1] = 'X';
+		}
+
+		break;
+	case 3:
+		if (array[0][2] == 'O' || array[0][2] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[0][2] = 'X';
+		}		
+		
+		break;
+	case 4:
+		if (array[1][0] == 'O' || array[1][0] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[1][0] = 'X';
+		}
+		break;
+	case 5:
+		if (array[1][1] == 'O' || array[1][1] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[1][1] = 'X';
+		}
+		break;
+	case 6:
+		if (array[1][2] == 'O' || array[1][2] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[1][2] = 'X';
+		}
+		break;
+	case 7:
+		if (array[2][0] == 'O' || array[2][0] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[2][0] = 'X';
+		}
+
+		break;
+	case 8:
+		if (array[2][1] == 'O' || array[2][1] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[2][1] = 'X';
+		}
+
+		break;
+	case 9:
+		if (array[2][2] == 'O' || array[2][2] == 'X') {
+			cout << "This position is occupied, try again." << endl;
+			inputPosition(array);
+		}
+		else {
+			array[2][2] = 'X';
+		}
+		break;
+	}
+
 }
 void alterDeposit(int* deposit, bool win) {
 	if (win) {
@@ -322,6 +657,15 @@ void printLetters(string input, char from, char to) {
 	printMessage(newString, false, false, ORDINARY_SIZE);
 }
 
+void drawArray(char array[][3]) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
+	//cout << "Choose Position:" << endl << "> ";
+}
 void printAvailableLetters(string initial) {
 	printMessage("Available letters:", true, true, ORDINARY_SIZE);
 	printLetters(initial, 'A', 'M');
